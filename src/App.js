ebb24e5e64cc3eceeb45d8c3b4f6b7be7c37bfef
Joey Rattana <rattanajoey@gmail.com>
2024-12-03 17:55:08 -0800

    Fix attemps domain name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import './App.css';
 import ShogiBoardComponent from './components/Shogi/ShogiBoardComponent';
 import SpeedDialComponent from './components/SpeedDial/SpeedDialComponent';
+import { Typography } from '@mui/material';
 
 const App = () => {
   const [selectedIcon, setSelectedIcon] = useState('Home');
@@ -11,6 +13,9 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className='App-header'>
+        <Typography variant='h4'>Akogare Cafe</Typography>
+      </div>
       {selectedIcon === 'Home' && 
       <ShogiBoardComponent />}
       <SpeedDialComponent onIconSelect={handleIconSelect} /> 
